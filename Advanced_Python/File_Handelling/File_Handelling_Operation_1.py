c589a2e5b0d1b8a6a8813ac19a78c67835aa93b1
Adebanjo Dara <adebanjoadeoluwadara@gmail.com>
2024-09-28 13:47:07 +0100

    File Handelling Operation1

--- Advanced_Python/File_Handelling/File_Handelling_Operation_1.py
+++ Advanced_Python/File_Handelling/File_Handelling_Operation_1.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("How are you?")
+#open the file
+file = open("Codingal_text.txt", "r")
+
+#read the content of the file
+print(file.read())
+
+#close the file
+file.close()
