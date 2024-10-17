14f77a02c1dd9a779200a4725724c930c03e9df0
Adebanjo Dara <adebanjoadeoluwadara@gmail.com>
2024-10-17 15:41:20 +0100

    Msg.py

--- Tkinter/Msg.py
+++ Tkinter/Msg.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 root = Tk()
 root.geometry('300x350')
 
+#then display your tkinter message
 def msg():
     messagebox.showwarning("Alert", "Stop, there is a virus!!")
 button = Button(root, text="Scan for virus", command=msg)
