660bd312ee4cba9e876db1aec7e5dd7b91eabde3
Adebanjo Dara <adebanjoadeoluwadara@gmail.com>
2024-10-18 14:04:26 +0100

    Tkinter_topwindow.py

--- Tkinter/Tkinter_topwindow.py
+++ Tkinter/Tkinter_topwindow.py
@@ -3,15 +3,16 @@ from PIL import Image, ImageTk
 
 root = Tk()
 root.title('image')
-root.geometry('300x350')
+root.geometry('500x500')
 
 #opening the image
-upload = Image.open("picf.jpg")
+upload = Image.open(r"C:\Users\HP\Desktop\Python_Codingal\Tkinter\picff.jpg")
+upload = upload.resize((370, 350), Image.Resampling.LANCZOS)
 
 #the image has to be tkinter compactible
 image = ImageTk.PhotoImage(upload)
 
-label = Label(root, image=image, height=350, width=370)
+label = Label(root, image=image)
 label.place(x=50, y=0)
 label2 = Label(root, text="This is how you add an image in tkinter")
 label2.place(x=40, y=360)
